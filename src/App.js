714288295c0406ded9d890c0016b4021350a79ac
marquitobb <maca9608@gmail.com>
2020-02-18 09:51:12 -0600

    ya quedo

--- src/App.js
+++ src/App.js
@@ -1,53 +1,8 @@
 import React, {Component} from 'react';
 import './style/App.css'
+import Form from './component/form';
 
 class App extends React.Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: '',
-      img: '#',
-    }
-  }
-  
-  /*
-  async componentDidMount(){
-    await this.fetchJale()
-  }*/
-
-  fetchJale = async () => {
-    //let res = await fetch('https://pokeapi.co/api/v2/pokemon/venusaur')
-    let res = await fetch(`https://pokeapi.co/api/v2/pokemon/${this.state.name}`)
-    let data = await res.json()
-
-    //console.log(data['sprites']['front_default'])
-    console.log(data.name);
-    console.log(data.sprites.front_default)
-    this.setState({
-      img: data.sprites.front_default
-    })
-  }
-  
-
-  handleName = event => {
-    this.setState({
-      name: event.target.value
-    })
-  }
-  
-
-  handleSubmit = event =>{
-    var jeje = this.state.name;
-    console.log(jeje)
-    alert(jeje)
-    this.fetchJale()
-    //limpiar
-    this.setState({
-      name: '',
-    })
-    event.preventDefault()
-  }
   
   render() {
     return (
@@ -66,19 +21,7 @@ class App extends React.Component{
         <img src={this.state.img}  alt="..."/>
         */}
 
-        <div className="card text-center">
-          <div className="card-header">
-            Featured
-          </div>
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-          <div className="card-footer text-muted">
-            2 days ago
-          </div>
-        </div>
+        <Form/>
       
       
         
